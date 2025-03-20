b0e37a5a6cdd0f0509fcf68a9404c32dfb579169
yoyoj1023 <bo.yo.chou@gmail.com>
2025-03-20 19:11:58 +0800

    19: scripts/* update

--- 19_Telephone/scripts/call_Telephone.js
+++ 19_Telephone/scripts/call_Telephone.js
@@ -1,21 +1,26 @@
 import hre from "hardhat";
 
-const telephone_address = "";
-const telephone_caller_address = "";
+// 請將以下地址替換成部屬後的實例合約地址
+const telephone_address = "0x45Df81613D3339C624cAD76FcD9DA5436a3c7FcD";
+const telephone_caller_address = "0xe9fd1041D58C975269a0f61EF42AEdA0b3e6a63A";
+console.log("目標 Telephone 實例合約地址:", telephone_address);
+console.log("目標 TelephoneCaller 實例合約地址:", telephone_caller_address);
 
-const [signer] = await hre.ethers.getSigner();
+const [signer] = await hre.ethers.getSigners();
 console.log("我的帳戶地址: ", signer.address);
+
 
 const telephone = await hre.ethers.getContractAt("Telephone", telephone_address);
 const telephone_caller = await hre.ethers.getContractAt("TelephoneCaller", telephone_caller_address);
 
 async function main() {
+    
     console.log("telephone owner: ", await telephone.owner());
     
     const tx = await telephone_caller.callTelephone();
     await tx.wait();
 
-    console.log("telephone owner: ", await telephone.owner());
+    console.log("Now, telephone owner: ", await telephone.owner());
     
 }
 
--- 19_Telephone/scripts/deploy_TelephoneCaller.js
+++ 19_Telephone/scripts/deploy_TelephoneCaller.js
@@ -3,12 +3,10 @@ import hre from "hardhat";
 async function main() {
     const TelephoneCaller = await hre.ethers.getContractFactory("TelephoneCaller");
     // 替換成 Telephone 的合約實例地址
-    const telephoneCaller = await TelephoneCaller.deploy("");
+    const telephoneCaller = await TelephoneCaller.deploy("0x45Df81613D3339C624cAD76FcD9DA5436a3c7FcD");
     await telephoneCaller.waitForDeployment();
     
     console.log("TelephoneCaller deployed to: ", await telephoneCaller.getAddress());
-    // 須測試
-    console.log("TelephoneCaller target: ", await telephoneCaller.target());
 }
 
 main().catch((error) => {
