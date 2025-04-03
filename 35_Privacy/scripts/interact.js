086c915ecbab564b360ca6031c9b37784deb289f
yoyoj1023 <bo.yo.chou@gmail.com>
2025-04-03 11:10:01 +0800

    35: update interact.js + log

--- 35_Privacy/scripts/interact.js
+++ 35_Privacy/scripts/interact.js
@@ -2,7 +2,7 @@ import hre from "hardhat";
 const { ethers } = hre;
 
 // 替換成隱私合約的關卡實例地址
-const privacy_address = "0x...";
+const privacy_address = "0x90F7A08218882619bb0D31cCBDc79fFF363184bD";
 
 async function main() {
 
@@ -12,22 +12,43 @@ async function main() {
     // uint256 public ID = block.timestamp;
     console.log("get ID 變數: ", await contract.ID());
 
-    console.log("#0 storageSlot: ", await ethers.provider.getStorage(privacy_address, 0));
-    console.log("#1 storageSlot: ", await ethers.provider.getStorage(privacy_address, 1));
-    console.log("#2 storageSlot: ", await ethers.provider.getStorage(privacy_address, 2));
-    console.log("#3 storageSlot: ", await ethers.provider.getStorage(privacy_address, 3));
-    console.log("#4 storageSlot: ", await ethers.provider.getStorage(privacy_address, 4));
-
-    console.log("#5 storageSlot: ", await ethers.provider.getStorage(privacy_address, 5));
-    console.log("#6 storageSlot: ", await ethers.provider.getStorage(privacy_address, 6));
-    console.log("#7 storageSlot: ", await ethers.provider.getStorage(privacy_address, 7));
-
     // 每個 slot 原則上可以保存  32 個位元組 的資料
     // 儲存緊湊 (storage packing) Solidity 會嘗試將多個小型的變數儲存在同一個儲存槽中，以節省 Gas 費用。 
-    // 這就是為什麼 bool、uint8 和 uint16 這些變數會被儲存在同一個儲存槽中。 
-    const storageSlot = 5;
 
-    // ...
+    // bool public locked = true;
+    console.log("#0 storageSlot: ", await ethers.provider.getStorage(privacy_address, 0));
+    
+    // uint256 public ID = block.timestamp; 佔用 32 bytes
+    console.log("#1 storageSlot: ", await ethers.provider.getStorage(privacy_address, 1));
+    
+    // uint8 private flattening = 10;
+    // uint8 private denomination = 255;
+    // uint16 private awkwardness = uint16(block.timestamp);
+    console.log("#2 storageSlot: ", await ethers.provider.getStorage(privacy_address, 2));
+    
+    // data[0]
+    console.log("#3 storageSlot: ", await ethers.provider.getStorage(privacy_address, 3));
+    // date[1]
+    console.log("#4 storageSlot: ", await ethers.provider.getStorage(privacy_address, 4));
+    // data[2]
+    console.log("#5 storageSlot: ", await ethers.provider.getStorage(privacy_address, 5));
+    
+    // 空
+    console.log("#6 storageSlot: ", await ethers.provider.getStorage(privacy_address, 6));
+    console.log("#7 storageSlot: ", await ethers.provider.getStorage(privacy_address, 7));
+ 
+    const storageSlot = 5;
+    const key32 = await ethers.provider.getStorage(privacy_address, storageSlot);
+    // Solidity 採用右側填充0、與右側截斷的方式來處理資料
+    const key16 = key32.slice(0, 16 * 2 + 2); // 16 bytes * 2 char + 2 char (0x)
+    console.log("key16: ", key16);
+
+    const tx = await contract.unlock(key16, { gasLimit: 1000000 });
+    console.log("tx: ", tx);
+    const receipt = await tx.wait();
+    console.log("receipt: ", receipt);
+
+    console.log("get locked 變數: ", await contract.locked());
 }
 
 main().catch((error) => {
