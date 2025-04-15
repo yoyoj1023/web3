2d084294eb6097cd8277c692a4cc53fcd96b773a
yoyoj1023 <bo.yo.chou@gmail.com>
2025-04-15 15:29:18 +0800

    49: update Staker.sol

--- 49_DeStakingApp/challenge-1-decentralized-staking/package/hardhat/contracts/Staker.sol
+++ 49_DeStakingApp/challenge-1-decentralized-staking/package/hardhat/contracts/Staker.sol
@@ -7,12 +7,26 @@ import "./ExampleExternalContract.sol";
 contract Staker {
     ExampleExternalContract public exampleExternalContract;
 
+    mapping ( address => uint256 ) public balances;
+
+    event Stake(address staker, uint256 amount);
+
+    uint256 public constant threshold = 1 ether;
+
     constructor(address exampleExternalContractAddress) {
         exampleExternalContract = ExampleExternalContract(exampleExternalContractAddress);
     }
 
     // Collect funds in a payable `stake()` function and track individual `balances` with a mapping:
     // (Make sure to add a `Stake(address,uint256)` event and emit it for the frontend `All Stakings` tab to display)
+    function stake(address staker, uint256 amount) public payable {
+        require(staker != address(0), "Invalid address.");
+        require(msg.value > 0, "Invalid payment.");
+        require(msg.value == amount, "msg.value != amount .");
+
+        balances[staker] += amount;
+        emit Stake(staker, amount);
+    }
 
     // After some `deadline` allow anyone to call an `execute()` function
     // If the deadline has passed and the threshold is met, it should call `exampleExternalContract.complete{value: address(this).balance}()`
@@ -22,4 +36,11 @@ contract Staker {
     // Add a `timeLeft()` view function that returns the time left before the deadline for the frontend
 
     // Add the `receive()` special function that receives eth and calls stake()
+    receive() external payable {
+        stake(msg.sender, msg.value);
+    }
+
+    function hello() public pure returns(uint) {
+        return 374;
+    }
 }
