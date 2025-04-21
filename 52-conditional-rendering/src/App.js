f4a46604c44eb556266c92f2f74f11347e878ca8
yoyoj1023 <bo.yo.chou@gmail.com>
2025-04-21 14:03:44 +0800

    Update App.js

--- 52-conditional-rendering/src/App.js
+++ 52-conditional-rendering/src/App.js
@@ -1,4 +1,4 @@
-function Item({ name, isPacked }) {
+function Item0({ name, isPacked }) {
   if (isPacked) {
     return <li className="item">{name} ✅</li>;
     // return null;
@@ -6,22 +6,65 @@ function Item({ name, isPacked }) {
   return <li className="item">{name}</li>;
 }
 
+
+function Item({ name, isPacked }) {
+  return (
+    <li className="item">
+      {isPacked ? name + '✅' : name}
+    </li>
+  );
+}
+
+function Item2({ name, isPacked }) {
+  return (
+    <li className="item">
+      {isPacked ? (
+        <del>
+          {name + '✅'}
+        </del>
+      ) : (
+        name
+      )}
+    </li>
+  );
+}
+
+function Item3({ name, isPacked }) {
+  return (
+    <li className="item">
+      {name} {isPacked && '✅'}
+    </li>
+  );
+}
+
+function Item4({ name, isPacked }) {
+  let itemContent = name;
+  if (isPacked) {
+    itemContent = name + " ✅";
+  }
+  return (
+    <li className="item">
+      {itemContent}
+    </li>
+  );
+}
+
 export default function PackingList() {
   return (
     <section>
       <h1>Sally Ride's Packing List</h1>
       <ul>
-        <Item 
-          isPacked={true} 
-          name="Space suit" 
+        <Item
+          isPacked={true}
+          name="Space suit"
         />
-        <Item 
-          isPacked={true} 
-          name="Helmet with a golden leaf" 
+        <Item
+          isPacked={true}
+          name="Helmet with a golden leaf"
         />
-        <Item 
-          isPacked={false} 
-          name="Photo of Tam" 
+        <Item
+          isPacked={false}
+          name="Photo of Tam"
         />
       </ul>
     </section>
