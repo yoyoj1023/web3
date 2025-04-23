763a04a87ce81ddbca89644d856340798295b250
yoyoj1023 <bo.yo.chou@gmail.com>
2025-04-23 14:29:04 +0800

    58: create *

---
+++ 58-keeping-components-pure/src/App.js
@@ -0,0 +1,21 @@
+function Cup({ guest }) {
+  return <h2>Tea cup for guest #{guest}</h2>;
+}
+
+export default function TeaSet() {
+  return (
+    <>
+      <Cup guest={1} />
+      <Cup guest={2} />
+      <Cup guest={3} />
+    </>
+  );
+}
+
+export function TeaGathering() {
+  let cups = [];
+  for (let i = 1; i <= 12; i++) {
+    cups.push(<Cup key={i} guest={i} />);
+  }
+  return cups;
+}
