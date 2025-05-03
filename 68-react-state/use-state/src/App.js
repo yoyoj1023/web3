ddd79d6015757b05f84e5c5c15a97f3ef0a7cbdb
yoyoj1023 <bo.yo.chou@gmail.com>
2025-05-03 22:48:34 +0800

    68: create *

---
+++ 68-react-state/use-state/src/App.js
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+import { sculptureList } from './data.js';
+
+export default function Gallery() {
+  // let index = 0;
+  const [index, setIndex] = useState(0);
+
+  function handleClick() {
+    // index = index + 1;
+    setIndex(index + 1);
+  }
+
+  let sculpture = sculptureList[index % sculptureList.length];
+  return (
+    <>
+      <button onClick={handleClick}>
+        Next
+      </button>
+      <h2>
+        <i>{sculpture.name} </i> 
+        by {sculpture.artist}
+      </h2>
+      <h3>  
+        ({index + 1} of {sculptureList.length})
+      </h3>
+      <img 
+        src={sculpture.url} 
+        alt={sculpture.alt}
+      />
+      <p>
+        {sculpture.description}
+      </p>
+    </>
+  );
+}
