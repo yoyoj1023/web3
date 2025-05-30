33cb61ac97731ef71abb0d2fe17c15e27cf46d6c
yoyoj1023 <bo.yo.chou@gmail.com>
2025-05-30 20:06:12 +0800

    82: first commit

---
+++ 82-react-ui-declaratively-input-state/02-02-identify-component-different-visual-states/src/App.js
@@ -0,0 +1,32 @@
+// Try 'submitting', 'error', 'success':
+export default function Form({ status = 'empty' }) {
+
+  if (status === 'success') {
+    return <h1>That's right!</h1>
+  }
+  return (
+    <>
+      <h2>City quiz</h2>
+      <p>
+        In which city is there a billboard that turns air into drinkable water?
+      </p>
+      <form>
+        <textarea disabled={
+          status === 'submitting'
+        } />
+        <br />
+        <button disabled={
+          status === 'empty' ||
+          status === 'submitting'
+        }>
+          Submit
+        </button>
+        {status === 'error' &&
+          <p className="Error">
+            Good guess but a wrong answer. Try again!
+          </p>
+        }
+      </form>
+    </>
+  );
+}
