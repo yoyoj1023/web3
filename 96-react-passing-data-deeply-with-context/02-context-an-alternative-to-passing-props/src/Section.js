c17bf200c65716ed4912af88f4f88cd5f81146e6
yoyoj1023 <32451691+yoyoj1023@users.noreply.github.com>
2025-06-13 09:32:13 +0800

    96: first commit

---
+++ 96-react-passing-data-deeply-with-context/02-context-an-alternative-to-passing-props/src/Section.js
@@ -0,0 +1,11 @@
+import { LevelContext } from './LevelContext.js';
+
+export default function Section({ level, children }) {
+  return (
+    <section className="section">
+      <LevelContext value={level}>
+        {children}
+      </LevelContext>
+    </section>
+  );
+}
