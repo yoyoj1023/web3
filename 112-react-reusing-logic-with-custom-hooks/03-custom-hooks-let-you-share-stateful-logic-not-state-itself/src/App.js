97a224daf85730accdc6714183d5de336d414f78
yoyoj1023 <32451691+yoyoj1023@users.noreply.github.com>
2025-07-15 09:14:19 +0800

    Add example for sharing logic with custom hooks in React
    Introduced a new React example demonstrating how to share stateful logic using a custom hook (useFormInput) without sharing state itself. Added supporting files including App.js, useFormInput.js, index.js, styles, and project configuration.

---
+++ 112-react-reusing-logic-with-custom-hooks/03-custom-hooks-let-you-share-stateful-logic-not-state-itself/src/App.js
@@ -0,0 +1,48 @@
+import { useFormInput } from './useFormInput.js';
+
+export default function Form() {
+  const firstNameProps = useFormInput('Mary');
+  const lastNameProps = useFormInput('Poppins');
+
+  return (
+    <>
+      <label>
+        First name:
+        <input {...firstNameProps} />
+      </label>
+      <label>
+        Last name:
+        <input {...lastNameProps} />
+      </label>
+      <p><b>Good morning, {firstNameProps.value} {lastNameProps.value}.</b></p>
+    </>
+  );
+}
+
+// 等效的
+export default function Form1() {
+  const [firstName, setFirstName] = useState('Mary');
+  const [lastName, setLastName] = useState('Poppins');
+
+  function handleFirstNameChange(e) {
+    setFirstName(e.target.value);
+  }
+
+  function handleLastNameChange(e) {
+    setLastName(e.target.value);
+  }
+
+  return (
+    <>
+      <label>
+        First name:
+        <input value={firstName} onChange={handleFirstNameChange} />
+      </label>
+      <label>
+        Last name:
+        <input value={lastName} onChange={handleLastNameChange} />
+      </label>
+      <p><b>Good morning, {firstName} {lastName}.</b></p>
+    </>
+  );
+}
